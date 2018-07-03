5f3a487a1c5213b8c1ddb4b668aa101d74aaa124
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-03 23:00:32 +0000

    accepts condition in command line

---
+++ vigenere/vigenere.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, string argk[]){
+    //string key;
+    if (argc > 0){
+    printf("key phrase: %s", argk[1]);
+    }
+}
