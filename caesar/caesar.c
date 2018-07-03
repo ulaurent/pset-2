f08602ea1783924d7c4c901d39bda58162a116c1
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-03 01:08:14 +0000

    changes user input into ASCII

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main (int argc, char *argk[]){
-    
-    if (argc > 0 && argk[1] > 0){
+int main (int argc, string argk[]){
+    string phrase;
+    if(argc > 1 && argc <= 2){
         printf("hello: %s \n", argk[1]);
     }
     else {
         printf("Invalid input");
     }
+
+   int i = atoi(argk[1]);
+   printf("Your key is %i\n", i);
+
+   phrase = get_string("What is your phrase: ");
+
+   for (int j =0; j < strlen(phrase); j++){
+       //printf("%c %i",phrase[j], (int)phrase[j]);
+       int fix = (((int)phrase[j]) + i);
+       char fix2 = (phrase[j]+i);
+       printf("%c %i\n",fix2, fix);
+
+   }
+
+
 }
