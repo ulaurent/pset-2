7dfff698633dfa3e3ee996206bd8664ceb1fbf10
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-04 13:26:14 +0000

    rough solution set

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -4,8 +4,20 @@
 #include <string.h>
 
 int main(int argc, string argk[]){
-    //string key;
-    if (argc > 0){
-    printf("key phrase: %s", argk[1]);
+    string plaintext;
+
+    if (argc > 1 && argc <= 2){
+    printf("key phrase: %s\n", argk[1]);
+    }
+    else{
+        printf("ERORR");
+    }
+
+    plaintext = get_string("Your text: ");
+
+    for (int j = 0; j < strlen(plaintext); j++){
+        for (int k = 0; k < strlen(argk); k++){
+       printf((((plaintext[j]+argk[1][k])-97)%26)+97);
+        }
     }
 }
