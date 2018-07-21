1bcb9cc8ddd99d61212229b8fe2f802102cedc60
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-21 14:55:24 +0000

    added revision with comments for clarity

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -1,3 +1,6 @@
+/* This program takes arguments to a cryptic message, and translates the users
+message based on those parameters*/
+
 #include <stdio.h>
 #include <cs50.h>
 #include <stdlib.h>
@@ -6,8 +9,10 @@
 
 int main(int argc, string argk[]){
 
+    // Introduced local variable within main function
     string plaintext;
 
+    // Add parameters to number of arguments accepted in the command line
     if (argc > 1 && argc <= 2){
     printf("key phrase: %s\n", argk[1]);
     }
@@ -15,10 +20,13 @@ int main(int argc, string argk[]){
         printf("ERORR INVALID INPUT\n");
     }
 
+    // Get string function from user and set it equal to the variable plaintext
     plaintext = get_string("What is your text: ");
 
     int j = 0;
 
+    // For loop to iterate through plaintext, and adjust j by ++ 1, through each iteration, so argument in
+    // command line can repeatedly iterated through
     for(int i=0 ; i < strlen(plaintext); i++){
 
         j = j % strlen(argk[1]);
